ca1d1703205c6f6d467edadc3b9ff8df27528fb0
okhalidfa <khalidhasan9953@gmail.com>
2026-04-11 09:22:43 +0600

    log in done

---
+++ style/index.js
@@ -0,0 +1,25 @@
+
+
+document.getElementById("log").addEventListener("click", function(){
+    
+    const ad= document.getElementById("add");
+    const at = ad.value;
+    const pas = document.getElementById("pass");
+    const pa= pas.value;
+    console.log(pa); 
+
+
+
+    if (at== "admin" && pa=="admin123"){
+      alert("log in successful");
+      window.location.assign("/style/home.html")
+    
+    } else {
+        alert("log in failed");
+        return;
+    }
+
+
+
+
+})
